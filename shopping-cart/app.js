31ef381c1ad7e69e3e732e224a0c4fefd1356f8f
James Taber <JATaber@fullsail.edu>
2018-06-06 15:48:32 -0500

    completed route grouping and protection

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -12,6 +12,7 @@ var flash = require('connect-flash');
 var validator = require('express-validator');
 
 var index = require('./routes/index');
+var userRoutes = require('./routes/user');
 
 var app = express();
 
@@ -36,6 +37,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next){
+  res.locals.login = req.isAuthenticated();
+  next();
+});
+
+app.use('/user', userRoutes);
 app.use('/', index);
 
 // catch 404 and forward to error handler

906477f1a58d4b9ffd6b7401d83ce70963ab3dab
James Taber <JATaber@fullsail.edu>
2018-06-06 17:51:12 -0500

    setting up cart view

--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -31,4 +31,13 @@ router.get('/shoppingCart/:id', function(req, res, next){
   });
 });
 
+router.get('/shopping-cart', function(req, res, next){
+  if (!req.session.cart) {
+    return res.render('shop/shopping-cart', {products: null});
+  }
+
+  var cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
+
 module.exports = router;

f9b954c710d155190a43e002d5ae6ff346ee3230
James Taber <JATaber@fullsail.edu>
2018-06-15 01:26:11 -0500

    fixed error in bootstrap

--- shopping-cart/public/javascripts/checkout.js
+++ shopping-cart/public/javascripts/checkout.js
@@ -19,9 +19,31 @@ var style = {
 };
 
 var card = elements.create('card', {style: style});
-card.mount('#checkout-form');
+card.mount('#card-element');
 
-$form.submit(function(e){
-  $form.find('button').prop('disabled', true);
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
 
+// Handle form submission.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error.
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server.
+      stripeTokenHandler(result.token);
+    }
+  });
 });
